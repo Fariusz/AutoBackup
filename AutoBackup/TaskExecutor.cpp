57d6b42ee45513ee9918b33cdcd5f23689e2e6bc
Radosław <61850108+Fariusz@users.noreply.github.com>
2021-02-02 23:03:26 +0100

    Poprawiono Error Handler

--- AutoBackup/TaskExecutor.cpp
+++ AutoBackup/TaskExecutor.cpp
@@ -174,8 +174,6 @@ void TaskExecutor::DoBackup(string source, string destination, bool compress)
 }
 void TaskExecutor::ErrorHandler(LPTSTR lpszFunction)
 {
-	// Retrieve the system error message for the last-error code.
-
 	LPVOID lpMsgBuf;
 	LPVOID lpDisplayBuf;
 	DWORD dw = GetLastError();
@@ -190,8 +188,6 @@ void TaskExecutor::ErrorHandler(LPTSTR lpszFunction)
 		(LPTSTR)&lpMsgBuf,
 		0, NULL);
 
-	// Display the error message.
-
 	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
 		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
 	StringCchPrintf((LPTSTR)lpDisplayBuf,
@@ -199,8 +195,6 @@ void TaskExecutor::ErrorHandler(LPTSTR lpszFunction)
 		TEXT("%s failed with error %d: %s"),
 		lpszFunction, dw, lpMsgBuf);
 	MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
-
-	// Free error-handling buffer allocations.
 
 	LocalFree(lpMsgBuf);
 	LocalFree(lpDisplayBuf);
